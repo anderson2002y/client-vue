d734968f2753da3f73c9d665d21e1d731488a5de
Anderson Sánchez <anderson2002y@gmail.com>
2025-04-09 13:56:02 -0600

    feat(table): display data in tables

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,7 +1,11 @@
 <script setup>
+  import axios from 'axios';
   import { FormKit } from '@formkit/vue';
+  import { useRouter } from 'vue-router';
   import  RouterLink  from '../components/UI/RouterLink.vue';
   import Heading from '../components/UI/Heading.vue';
+
+  const router = useRouter();
 
   defineProps({
     titulo: {
@@ -10,7 +14,12 @@
   });
 
   const handleSubmit = (data) => {
-    console.log(data);
+    axios.post('http://localhost:4000/clientes', data)
+      .then(respuesta => {
+        //Redireccionar
+        router.push({ name: 'listado-clientes'});
+      })
+      .catch(error => console.log(error))
   }
 
 </script>
@@ -18,8 +27,8 @@
 <template>
   <div>
     <div class="flex justify-end"> 
-      <RouterLink to="inicio">
-        Inicio
+      <RouterLink to="listado-clientes">
+        Volver
       </RouterLink>
     </div>
     <Heading> {{ titulo }}</Heading>
