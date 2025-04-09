d3a64a98d8ef660472e774ce89e834b319dc4095
Anderson Sánchez <anderson2002y@gmail.com>
2025-04-09 12:18:24 -0600

    feat(api): query API for data retrieval

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,16 @@
 <script setup>
+  import { onMounted, ref } from 'vue';
+  import axios from 'axios';
   import  RouterLink  from '../components/UI/RouterLink.vue';
   import Heading from '../components/UI/Heading.vue';
+
+  const clientes = ref([]);
+
+  onMounted(() => {
+    axios('http://localhost:4000/clientes')
+      .then(({ data }) => clientes.value = data)
+      .catch(error => console.log('Hubo un error'))
+  });
 
   defineProps({
     titulo: {
